24bab85ef9c31ea5a0a5f27f74ff8df468fcec91
santhra-code <Santhrasatheesh2005@gmail.com>
2026-08-31 19:51:44 +0530

    Polish storefront theme and project configuration

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "../app/generated/prisma/client";
+import { PrismaClient } from "@/app/generated/prisma/client";
 import { PrismaNeon } from "@prisma/adapter-neon";
 import bcrypt from "bcryptjs";
 
